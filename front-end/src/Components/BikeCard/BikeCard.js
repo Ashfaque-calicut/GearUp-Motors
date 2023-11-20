e913c4d11b3a78efde889c2ec77a192ce7eda815
Ashfaque-calicut <ashfaquepalazhi@gmail.com>
2023-11-20 21:09:39 +0530

    update Banner

--- front-end/src/Components/BikeCard/BikeCard.js
+++ front-end/src/Components/BikeCard/BikeCard.js
@@ -42,7 +42,7 @@ const BikeCard = () => {
   // console.log(token);
   const cardStyle = {
     width: '400px',
-    height: role === 2 ? '400px' : '400px', 
+    height: role === 2 ? '500px' : '600px', 
   }
 
   return (
