6b41227147377d82a05c3d0bf1f1ce95e3c0b99e
Ashfaque-calicut <ashfaquepalazhi@gmail.com>
2023-11-24 20:30:16 +0530

    updated Header

--- front-end/src/Components/About/About.js
+++ front-end/src/Components/About/About.js
@@ -12,25 +12,27 @@ const About = () => {
       </div>
       <div className="about-content">
         <h2>About Us</h2>
-        <p>
-          We are a passionate team of cyclists dedicated to promoting a healthy and active lifestyle through biking.
-        </p>
-        <p>
-          At Bike Showroom, we believe that cycling is more than just a mode of transportation; it's an adventure, a sport,
-          and a way to connect with the outdoors.
-        </p>
-        <p>
-          Our showroom offers a wide range of bikes catering to every type of rider, from beginners to professionals.
-          Whether you're into road biking, mountain biking, or urban commuting, we have the perfect bike for you.
-        </p>
-        <p>
-          Our mission is to provide top-quality bikes, exceptional customer service, and a welcoming environment for all
-          cyclists. We are committed to helping you find the right bike and accessories to make your biking experience truly
-          enjoyable.
-        </p>
-        <p>
-          Visit us today to experience the thrill of exploring the world on two wheels!
-        </p>
+        <div className="about-text">
+          <p>
+            We are a passionate team of cyclists dedicated to promoting a healthy and active lifestyle through biking.
+          </p>
+          <p>
+            At Bike Showroom, we believe that cycling is more than just a mode of transportation; it's an adventure, a sport,
+            and a way to connect with the outdoors.
+          </p>
+          <p>
+            Our showroom offers a wide range of bikes catering to every type of rider, from beginners to professionals.
+            Whether you're into road biking, mountain biking, or urban commuting, we have the perfect bike for you.
+          </p>
+          <p>
+            Our mission is to provide top-quality bikes, exceptional customer service, and a welcoming environment for all
+            cyclists. We are committed to helping you find the right bike and accessories to make your biking experience truly
+            enjoyable.
+          </p>
+          <p>
+            Visit us today to experience the thrill of exploring the world on two wheels!
+          </p>
+        </div>
       </div>
     </section>
   );
--- front-end/src/Components/Header/Header.js
+++ front-end/src/Components/Header/Header.js
@@ -2,92 +2,97 @@ import React, { useState } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 
-
 const Header = () => {
   const [token, setToken] = useState(sessionStorage.getItem('token'));
   const role = sessionStorage.getItem('role');
-
+  const [menuVisible, setMenuVisible] = useState(false);
 
   const handleLogout = () => {
     sessionStorage.removeItem('token');
     setToken(null);
   };
 
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (
     <header className='header'>
       <div className='logo'>
-      <span style={{color:'blue'}}> GearUp</span>  <span style={{ color: 'red' }}>Motors</span>
-       
+        <span style={{ color: 'blue' }}> GearUp</span> <span style={{ color: 'red' }}>Motors</span>
       </div>
-      <nav className='nav'>
+      <div className={`nav-toggle ${menuVisible ? 'active' : ''}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav className={`nav ${menuVisible ? 'active' : ''}`}>
         <ul>
           <li>
-            <Link to='/' className='nav-link'>
+            <Link to='/' className='nav-link' onClick={toggleMenu}>
               Home
             </Link>
           </li>
           {token && (
             <li>
-              <Link to='/Bike' className='nav-link'>
+              <Link to='/Bike' className='nav-link' onClick={toggleMenu}>
                 Bikes
               </Link>
             </li>
           )}
-          {role==2 && (
+          {role == 2 && (
             <li>
-              <Link to='/Booking' className='nav-link'>
+              <Link to='/Booking' className='nav-link' onClick={toggleMenu}>
                 Booking
               </Link>
             </li>
           )}
-          {
-            role==2 &&(
-              <li>
-                <Link to='/user-booking' className='nav-link'>
-                  BookingDetails
-                </Link>
-              </li>
-            )
-          }
+          {role == 2 && (
+            <li>
+              <Link to='/user-booking' className='nav-link' onClick={toggleMenu}>
+                BookingDetails
+              </Link>
+            </li>
+          )}
           {role == 1 && (
             <li>
-              <Link to='/bookingDetails' className='nav-link'>
+              <Link to='/bookingDetails' className='nav-link' onClick={toggleMenu}>
                 BookingDetails
               </Link>
             </li>
           )}
           <li>
-            <Link to='/About' className='nav-link'>
+            <Link to='/About' className='nav-link' onClick={toggleMenu}>
               About Us
             </Link>
           </li>
           <li>
-            <Link to='/contact' className='nav-link'>
+            <Link to='/contact' className='nav-link' onClick={toggleMenu}>
               Contact
             </Link>
           </li>
-          {token &&(
+          {token && (
             <li>
-              <Link to='/profile' className='nav-link'>
-              Profile
-              </Link> 
+              <Link to='/profile' className='nav-link' onClick={toggleMenu}>
+                Profile
+              </Link>
             </li>
           )}
           {token ? (
             <li>
-              <Link to='/Login' className='nav-link' onClick={handleLogout}>
+              <Link to='/Login' className='nav-link' onClick={() => { handleLogout(); toggleMenu(); }}>
                 Logout
               </Link>
             </li>
           ) : (
             <>
               <li>
-                <Link to='/Login' className='nav-link'>
+                <Link to='/Login' className='nav-link' onClick={toggleMenu}>
                   Login
                 </Link>
               </li>
               <li>
-                <Link to='/Register' className='nav-link'>
+                <Link to='/Register' className='nav-link' onClick={toggleMenu}>
                   Register
                 </Link>
               </li>
@@ -100,4 +105,3 @@ const Header = () => {
 };
 
 export default Header;
-
